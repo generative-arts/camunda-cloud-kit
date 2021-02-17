5db69f9994dfb99effce6733b5540a98b0d9eeae
Adam <i@adamurban.de>
2021-02-17 13:23:49 +0100

    add camundacloud controller

---
+++ src/controller/camundacloud.controller.ts
@@ -0,0 +1,26 @@
+import { logger } from '../utils/Logger'
+import { AddCanvasElementWorker } from '../worker/addcanvaselement.worker'
+import { BpmnLoaderWorker } from '../worker/bpmnloader.worker'
+import { DecreaseWorker } from '../worker/decrease.worker'
+import { ZeebeController } from './zeebe.controller'
+
+export class CamundaCloudController {
+  public static async run(seconds: number) {
+    logger.info(`Connecting Zeebe Client`)
+    const zeebeController = new ZeebeController()
+    await zeebeController.getTopology()
+
+    logger.info(`Creating Zeebe Workers`)
+
+    const addCanvasElementWorker = new AddCanvasElementWorker(zeebeController)
+    addCanvasElementWorker.create()
+
+    const decreaseWorker = new DecreaseWorker(zeebeController)
+    decreaseWorker.create()
+
+    const bpmnLoaderWorker = new BpmnLoaderWorker(zeebeController)
+    bpmnLoaderWorker.create()
+
+    await zeebeController.close(seconds)
+  }
+}
